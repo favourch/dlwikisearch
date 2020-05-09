dd4a305cfe9f68d991592f642daf01cc9e2cd584
Dev-Geek <34694992+Dev-Geek@users.noreply.github.com>
2020-05-09 14:36:21 +0100

    Key update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const options = {
   AT: {
     sandbox: true, //false if going to prod
-    apiKey: "533e31f426eeaad116badc21aaf5e5fe3b5d7272775957948e0b10bd1a8ba13f", //Your Africas Talking Sandox or Live Key
+    apiKey: "e7wetwet87te987tf9wt7e9fw7tf9q7tef9qt9qet97tf97q97ef97q97", //Your Africas Talking Sandox or Live Key
     username: "sandbox", // Your Username: "sandbox" for testing...
     format: "json",
   },
