5350668c7338f918f815dc02fb47e6c9eaafec47
Favour Chukwuedo <fchukwuedo@gmail.com>
2020-06-20 19:39:34 +0100

    first commit 2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const options = {
   AT: {
-    sandbox: true, //false if going to prod
+    sandbox: false, //false if going to prod
     apiKey: "0de8c3196461a914c3136e0e82186db81a43a2a23eb0e15d323dfce8b7a8edbb", //Your Africas Talking Sandox or Live Key
     username: "sandbox", // Your Username: "sandbox" for testing...
     format: "json",
